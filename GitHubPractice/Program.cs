11cf053137193fb42e683de09f18a0983f0505e4
Dad  S Wonkulah Jr <dadwonkulahjr96@gmail.com>
2020-09-11 12:49:40 +0000

    Added a new Console csproj file

---
+++ GitHubPractice/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace GitHubPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int age = 24;
+            Console.WriteLine($"My name is Tuse i am {age.ToString()} years old.");
+          
+        }
+    }
+}
