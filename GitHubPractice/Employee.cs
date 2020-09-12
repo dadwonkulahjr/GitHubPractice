fb58f2f0129c92e95e240fac72c7781a2b4ad7f4
Dad S Wonkulah Jr <68390101+dadwonkulahjr@users.noreply.github.com>
2020-09-12 15:19:19 +0000

    Added an Employee class file

---
+++ GitHubPractice/Employee.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GitHubPractice
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public Employee(int id, string name, string email)
+        {
+            Id = id;
+            Name = name;
+            Email = email;
+        }
+
+        public void PrintInfo()
+        {
+            Console.WriteLine($"Id = {Id}");
+            Console.WriteLine($"Name = {Name}");
+            Console.WriteLine($"Email = {Email}");
+        }
+    }
+}
